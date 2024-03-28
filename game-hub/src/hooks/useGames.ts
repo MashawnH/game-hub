950c5d2e75368ed6e598c9f502c8fa1e181c4a3b
Mashawn <h.mashawn2@gmail.com>
2024-03-28 15:27:50 -0700

    show load skeletons

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import apiClients from "../services/api-clients";
 import { CanceledError } from "axios";
 
@@ -26,20 +26,26 @@ export interface Platform {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [isLoading, setLoading] = useState(false);
   
     useEffect(() => {
         const controller = new AbortController();
-      apiClients
+        setLoading(true);
+
+        apiClients
         .get<FetchGamesResponse>("/games", {signal: controller.signal})
-        .then((res) => setGames(res.data.results))
+        .then((res) => {
+          setGames(res.data.results)
+          setLoading(false);})
         .catch((err) => {
             if (err instanceof CanceledError) return;
-            setError(err.message)}
-            );
+            setError(err.message)
+            setLoading(false);
+          });
     return () => controller.abort();
     }, []);
 
-    return {games, error};
+    return {games, error, isLoading};
 }
 
 export default useGames
