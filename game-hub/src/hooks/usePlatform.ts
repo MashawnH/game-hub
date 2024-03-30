cef95abf6b90069acf763aa52db252826f524427
Mashawn <h.mashawn2@gmail.com>
2024-03-30 11:59:51 -0700

    build platform selector

---
+++ game-hub/src/hooks/usePlatform.ts
@@ -0,0 +1,24 @@
+import useData from "./useData";
+import { Genre } from "./useGenres";
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+ export interface Game {
+    id: number;
+    name: string;
+    background_image: string;
+    parent_platforms: {platform: Platform[]};
+    metacritic: number;
+
+  }
+  
+
+
+const usePlatform = () => useData<Platform>("/platforms/lists/parents");
+   
+
+export default usePlatform
